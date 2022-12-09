32aa419da15acf20240c2a0accd08d12bf6134c9
Katherine de Vay <katdevay2@gmail.com>
2022-12-09 12:33:34 -0600

    Add basic formatting

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+const states = [
+    ['alabama', 'AL'],
+    ['alaska', 'AK'],
+    ['arizona', 'AZ'],
+    ['arkansas', 'AR'],
+    ['california', 'CA'],
+    ['colorado', 'CO'],
+    ['connecticut', 'CT'],
+    ['delaware', 'DE'],
+    ['florida', 'FL'],
+    ['georgia', 'GA'],
+    ['hawaii', 'HI'],
+    ['idaho', 'ID'],
+    ['illinois', 'IL'],
+    ['indiana', 'IN'],
+    ['iowa', 'IA'],
+    ['kansas', 'KS'],
+    ['kentucky', 'KY'],
+    ['louisiana', 'LA'],
+    ['maine', 'ME'],
+    ['maryland', 'MD'],
+    ['massachusetts', 'MA'],
+    ['michigan', 'MI'],
+    ['minnesota', 'MN'],
+    ['mississippi', 'MS'],
+    ['missouri', 'MO'],
+    ['montana', 'MT'],
+    ['nebraska', 'NE'],
+    ['nevada', 'NV'],
+    ['new Hampshire', 'NH'],
+    ['new Jersey', 'NJ'],
+    ['new Mexico', 'NM'],
+    ['new York', 'NY'],
+    ['north Carolina', 'NC'],
+    ['north Dakota', 'ND'],
+    ['ohio', 'OH'],
+    ['oklahoma', 'OK'],
+    ['oregon', 'OR'],
+    ['pennsylvania', 'PA'],
+    ['rhode Island', 'RI'],
+    ['south Carolina', 'SC'],
+    ['south Dakota', 'SD'],
+    ['tennessee', 'TN'],
+    ['texas', 'TX'],
+    ['utah', 'UT'],
+    ['vermont', 'VT'],
+    ['virginia', 'VA'],
+    ['washington', 'WA'],
+    ['west Virginia', 'WV'],
+    ['wisconsin', 'WI'],
+    ['wyoming', 'WY'] 
+];
+
 const weatherCodes = {
     200: 'thunderstorm and light rain',
     230: 'thunderstorm and light rain',
@@ -104,10 +157,20 @@ const windDirection = {
     }
 };
 
-function findDescription (id) {
-    return weatherCodes[id];
+function findState(string){
+    string = string.toLowerCase();
+    for (let i = 0; i < 50; i++){
+        if (string === states[i][0]){
+            return states[i][1];
+        }
+    }
+    return false;
 }
 
+function findDescription(id) {
+    return weatherCodes[id];
+}
+const heading = document.getElementById('location');
 const errMessage = document.getElementById('error');
 const search = document.getElementById('search');
 const city = document.getElementById('city');
@@ -123,7 +186,9 @@ const weather = [
     document.getElementById('temp'), 
     document.getElementById('feelsLike'), 
     document.getElementById('high'), 
-    document.getElementById('low')
+    document.getElementById('low'),
+    document.getElementById('wUnit'),
+    document.getElementById('pUnit')
 ];
 
 function clearDisplay(element) {
@@ -133,57 +198,101 @@ weather.forEach(clearDisplay);
 clearDisplay(errMessage);
 icon.style.display = 'none'
 
-const APIs = {
+const urls = {
     geo: {start: 'http://api.openweathermap.org/geo/1.0/direct?q=', end: '&limit=1&appid=d0a94ad4220c8e5634a0b81bf7e35a30'},
     weather: {start: 'https://api.openweathermap.org/data/2.5/weather?lat=', mid: '&lon=', end:'&appid=d0a94ad4220c8e5634a0b81bf7e35a30&units=imperial'},
     icon: {start: 'http://openweathermap.org/img/wn/', end: '@2x.png'}
 };
-// async function getCoordinates(){
-// geocode lookup: replace spaces with '_'
-// http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',' + state + ',' + country + '&limit=1&appid=d0a94ad4220c8e5634a0b81bf7e35a30
-//d0a94ad4220c8e5634a0b81bf7e35a30
-//https://api.openweathermap.org/data/2.5/weather?lat=29.950793lon=-90.075713&appid=d0a94ad4220c8e5634a0b81bf7e35a30&units=imperial
-// icon url format: 'http://openweathermap.org/img/wn/' + data.weather.icon + '@2x.png'
 
 async function getWeather() {
+    weather.forEach(clearDisplay);
+    clearDisplay(errMessage);
     try {
-        const cityName = !city.value ? '' : city.value;
-        const stateName = !state.value ? '' : state.value;
+        // Format user inputs
+        let stateCode, displayLocation, searchTerm, tempName;
+        const cityName = !city.value ? '' : city.value.replaceAll(' ', '_');
+        const stateName = !state.value ? '' : state.value.replaceAll(' ', '_');
         const countryName = country.value;
+        // Require country name
         if (!countryName){
-            // display error message
+            errMessage.textContent = 'Country name required.'
             return;
+        // If searching US, find state code for URL
+        } else if (countryName === 'US' && stateName !== '') {
+            stateCode = findState(stateName);
+            if (!stateCode){
+                errMessage.textContent = 'No results were found for this state name: "' + stateName + '".';
+                return;
+            }
+            searchTerm = cityName + ',' + stateCode + ',' + countryName ;
+        // If searching outside of US use state name in URL
+        } else {
+            searchTerm = cityName + ',' + stateName + ',' + countryName;
         }
-        const searchTerm = cityName + stateName + countryName;
-        const geoResponse = await fetch( APIs.geo.start + searchTerm + APIs.geo.end, {mode: 'cors'})
+
+        // Find location's latitude / longitude
+        const geoResponse = await fetch( urls.geo.start + searchTerm + urls.geo.end, {mode: 'cors'})
         const geoData = await geoResponse.json()
-        console.log(geoData);
-        // const lat = 
-        // const lon = 
-        // const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=d0a94ad4220c8e5634a0b81bf7e35a30&units=imperial', {mode: 'cors'})
-        // const data = await response.json()
-        // weather[0].textContent = findDescription(data.weather[0].id);
-        // weather[1].textContent = Math.round(data.wind.speed) + ' ' + units.imp.speed;
-        // if (data.wind.speed === '0'){ // If wind speed is 0
-        //     weather[2].style.display = 'none'; // Hide wind direction
-        // } else {
-        //     weather[2].textContent = windDirection.findQuadrant(data.wind.deg);
-        // }
-        // weather[3].textContent = data.main.humidity + '%'
-        // // Convert air pressure from millibar to inHG
-        // let airPress = Math.round(data.main.pressure / 33.864);
-        // weather[4].textContent = airPress + ' ' + units.pressure.mercury;
-        // weather[5].textContent = Math.round(data.main.temp) + '°' + units.imp.temp;
-        // weather[6].textContent = Math.round(data.main.feels_like) + '°';
-        // weather[7].textContent = Math.round(data.main.temp_max) + '°';
-        // weather[8].textContent = Math.round(data.main.temp_min) + '°';
-        // let imgSrc = 'http://openweathermap.org/img/wn/' + data.weather[0].icon  + '@2x.png';
-        // icon.src = imgSrc;
-        // icon.style.display = 'block';
+        const lat = geoData[0].lat;
+        const lon = geoData[0].lon;
+
+        // Find location's weather
+        const response = await fetch(urls.weather.start + lat + urls.weather.mid + lon + urls.weather.end, {mode: 'cors'})
+        const data = await response.json()
+
+        // Add weather data to display
+        weather[0].textContent = findDescription(data.weather[0].id); // description
+        weather[1].textContent = Math.round(data.wind.speed) + ' '; // wind speed
+        weather[3].textContent = data.main.humidity + '%'; // humidity
+        weather[5].textContent = Math.round(data.main.temp) + '°' + units.imp.temp;
+        weather[6].textContent = Math.round(data.main.feels_like) + '°';
+        weather[7].textContent = Math.round(data.main.temp_max) + '°';
+        weather[8].textContent = Math.round(data.main.temp_min) + '°';
+        weather[9].textContent = units.imp.speed;
+        weather[10].textContent = units.pressure.mercury;
+        
+        // If wind speed is 0 hide wind direction
+        if (data.wind.speed === '0'){ 
+            weather[2].style.display = 'none'; 
+        } else { // Display wind direction
+            weather[2].textContent = windDirection.findQuadrant(data.wind.deg);
+        }
+        
+        // Convert air pressure from millibar to inHG
+        let airPress = Math.round(data.main.pressure / 33.864);
+        weather[4].textContent = airPress + ' ';
+
+        // Display weather icon
+        let imgSrc = urls.icon.start + data.weather[0].icon  + urls.icon.end;
+        icon.src = imgSrc;
+        icon.style.display = 'block';
+
+        // Display location name
+        tempName = document.getElementById(countryName).textContent;
+        if (cityName === '' && stateName === ''){
+            displayLocation = tempName;
+        } else if (cityName !== '' && stateName !== '') {
+            displayLocation = cityName.replaceAll('_', ' ') + ', ' + stateName;
+        } else if (cityName !== '' && stateName === ''){
+            displayLocation = cityName.replaceAll('_', ' ') + ', ' + tempName;
+        } else if (cityName === '' && stateName !== ''){
+            displayLocation = stateName.replaceAll('_', ' ') + ', ' + tempName;
+        }
+        heading.textContent = displayLocation;
+
     } catch {
         errMessage.textContent = "No results were found for these search terms."
     }
 }
 
 search.addEventListener('click', getWeather);
-
+// API key:
+// d0a94ad4220c8e5634a0b81bf7e35a30
+// geocode API:
+// http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',' + state + ',' + country + '&limit=1&appid=d0a94ad4220c8e5634a0b81bf7e35a30
+// weather API:
+// https://api.openweathermap.org/data/2.5/weather?lat=29.950793lon=-90.075713&appid=d0a94ad4220c8e5634a0b81bf7e35a30&units=imperial
+// icon url format: 
+// 'http://openweathermap.org/img/wn/' + data.weather.icon + '@2x.png'
+// https://api.openweathermap.org/data/2.5/weather?lat=-33.8698439&lon=151.2082848&appid=d0a94ad4220c8e5634a0b81bf7e35a30&units=imperial
+// lat":,"lon":151.2082848,
